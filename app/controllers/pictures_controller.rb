1ce42214e35c2ca08e5d7f0c27abf028a45f35f6
Rinkal <rthakker@chicagobooth.edu>
2019-02-09 20:06:05 +0000

    v2

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,22 @@ class PicturesController < ApplicationController
   def index
     render("pic_templates/all_photos.html.erb")
   end
+  def new_form
+    render("pic_templates/new_form.html.erb")
+  end
+  def create_row
+    render("pic_templates/create_row.html.erb")
+  end
+  def show_details
+    render("pic_templates/show_details.html.erb")
+  end
+  def edit_form
+    render("pic_templates/edit_form.html.erb")
+  end
+  def update_row
+    render("pic_templates/update_row.html.erb")
+  end
+  def destroy_row
+    render("pic_templates/destroy_row.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   
-  get("/photos/new", {:controller => "pictures", :action =>""})
-  get("/create_photo", {:controller =>"pictures", :action => ""})
+  
   get("/photos", {:controller => "pictures", :action => "index"})
-  get("/photos/2431/edit", {:controller => "pictures", :action => ""})
-  
-  
-  
+  get("/photos/new", {:controller => "pictures", :action =>"new_form"})
+  get("/create_photo", {:controller =>"pictures", :action => "create_row"})
+  get("/photos/:an_id_number", {:controller => "pictures", :action => "show_details"})
+  get("/photos/:some_id_number/edit", {:controller =>"pictures", :action => "edit_form"})
+  get("/update_photo/:the_id_number", {:controller =>"pictures", :action => "update_row"})
+  get("/delete_photo/:bye_id_number", {:controller =>"pictures", :action => "destroy_row"})
+   
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
